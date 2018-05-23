444a7a0b96568d4a192d9ac8c4a2818533fb6094
Sam <sam@CIS2L0Y3HTDH.local>
2018-05-23 12:32:40 -0400

    updating ads.py

--- ads.py
+++ ads.py
@@ -28,7 +28,7 @@ def removeAds(im_bw, file):
         #gray = cv2.cvtColor(original, cv2.COLOR_BGR2GRAY)
     #else:
         #gray = original
-    do_diagnostics = False
+    do_diagnostics = True
     height,width = im_bw.shape[:2]
     cv2.rectangle(im_bw,(0,0),(width,height), (255,255,255), 100)
     im_bw_copy = im_bw.copy()
@@ -54,6 +54,8 @@ def removeAds(im_bw, file):
             right = max([vertex[0][0] for vertex in cnt])
             if (w > (width / 2)) and (bottom < (height * 0.75)):
                 cv2.rectangle(im_bw,(0,0),(width,bottom), (255,255,255), -1)
+            if (w > (width / 2)) and (top > (height * 0.85)):
+                cv2.rectangle(im_bw,(0,top),(width,height), (255,255,255), -1)
             #if (w > (width / 2)) and (top > (height * 0.90)):
                 #cv2.rectangle(im_bw,(0,0),(width,bottom), (255,255,255), -1)
             if (w * h) < 0.9 * width * height:
@@ -75,13 +77,6 @@ def removeAds(im_bw, file):
     #cv2.imwrite(os.path.join(nDirectory, file), original)
     if do_diagnostics:
         cv2.imwrite(os.path.join('no_ads', file.partition('.png')[0] + '.white.jpg'), white_image)
-    yi = height-50
-    while cv2.countNonZero(white_image[yi,:]) < (0.7*float(width)) and yi > (0.95*float(height)):
-        yi -= 1
-    #yi -= int(height/300)
-    if do_diagnostics:
-        print('Bottom cutoff: ' + str(100.0*float(yi)/float(height)))
-    cv2.rectangle(im_bw,(0,yi),(width,height), (255,255,255), -1)
     if do_diagnostics:
         cv2.imwrite(os.path.join('no_ads', file.partition('.png')[0] + '.contours.jpg'), blank_image)
     return im_bw
@@ -92,17 +87,17 @@ def noAds(image, area):
     return noAds
 
 def get_binary(file):
-    do_plots = False
+    do_plots = True
     t1 = time.time()
     original = cv2.imread(file, 0)
     t2 = time.time()
     #print('Image read time: ' + str(round(t2-t1, 2)) + ' s')
     h, w = original.shape[:2]
     hist_raw,bins = np.histogram(original.ravel(),256,[0,256])
-    hist = pd.Series(hist_raw[:200]).rolling(10).mean()
+    hist = pd.Series(hist_raw[:200]).rolling(20).mean().rolling(10).mean()
     h_total = hist.sum()
     h_cumulative = hist.cumsum()/h_total
-    h_grad = pd.Series(np.gradient(hist, 5)).rolling(5).mean()
+    h_grad = pd.Series(np.gradient(hist, 30)).rolling(30).mean()
     if do_plots:
         fig = plt.figure()
         plt.plot(hist)
@@ -116,14 +111,14 @@ def get_binary(file):
     g_cutoff = h_grad.min()*0.15
     while threshold<175 and h_grad.iloc[threshold] < g_cutoff:
         threshold += 1
-    #print(threshold)
+    print(threshold)
     if do_plots:
         gfig = plt.figure()
         plt.plot(h_grad)
         plt.plot([0,200],[g_cutoff,g_cutoff])
         gfig.savefig(file.partition('.png')[0] + '.grayscale_grad.pdf', bbox_inches='tight')
         plt.close(gfig)
-    original_padded = cv2.copyMakeBorder(original[:,0:w-50],10,10,10,10, cv2.BORDER_CONSTANT, value=255)
+    original_padded = cv2.copyMakeBorder(original[:,:],10,10,10,10, cv2.BORDER_CONSTANT, value=255)
     im_bw = cv2.threshold(original_padded, threshold, 255, cv2.THRESH_BINARY)[1]
     return im_bw
 
@@ -133,7 +128,7 @@ def process_image(file):
     im_bw = get_binary(file)
     t2 = time.time()
     #print('Binary conversion time: ' + str(round(t2-t1, 2)) + ' s')
-    #cv2.imwrite(os.path.join('no_ads', 'bw_test.png'), im_bw)
+    cv2.imwrite(os.path.join('no_ads', file + 'bw_test.jpg'), im_bw)
     #im = cleanImage(original)
     t1 = time.time()
     removeAds(im_bw, file)
@@ -152,8 +147,10 @@ def rmAds(folder):
     os.chdir(scans)
     if not os.path.exists(nDirectory):
         os.mkdir(nDirectory)
-    pool = Pool(4)
-    pool.map(process_image, sorted(glob.glob("*.png"), key=naturalSort))
+    #pool = Pool(4)
+    #pool.map(process_image, sorted(glob.glob("*.png"), key=naturalSort))
+    for file in sorted(glob.glob("*.png"), key=naturalSort):
+        process_image(file)
 
 
 
