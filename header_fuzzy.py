83c9d9b34a4d5d0502f841d7bf3a64fd7bc1a70e
neevparikh <neev_parikh@brown.edu>
2018-12-18 22:09:50 -0500

    Added the code for the cleaning of headers

--- header_fuzzy.py
+++ header_fuzzy.py
@@ -1,56 +1,74 @@
 import pandas as pd
 import pickle as pkl
 from fuzzywuzzy import fuzz
-
+import string
 
 def preprocess(df):
     # Preprocesses the ocr header
     try:
         map_dict = pkl.load(open('header_match_dict.pkl', 'rb'))
     except:
+        print("WARNING: pickle load failed")
         map_dict = {}
-    header_list = list(set(df["header"]) - set(map_dict.keys()))
+    header_list = list(set(df["Header"]) - set(map_dict.keys()))
     return header_list, map_dict
 
 
+common_errors = {
+    "0": "O",
+    "1": "l",
+    "5": "S",
+    "8": "B"
+}
+
+
+def clean_header(header):
+    cleaned_word = []
+    for let in header.upper().split():
+        if let in common_errors:
+            let = common_errors[let]
+        cleaned_word.append(let)
+    return string.join(cleaned_word, "")
+
 def clean(header_list):
     # Cleans the ocr header
-    return header_list
+    new_list = [clean_header(header) for header in header_list]
+    return new_list
 
 
 def score(string1, string2):
+    print(string1, string2)
     # Scores the fuzzy match between the true header and the ocr header
-    words1 = string1.upper().split()
-    words2 = string2.upper().split()
-    # if words1[-1] != words2[-1]:
-    #     if words2[-1] in sts:
-    #         words2 = words2[:-1]
-    #     if words1[-1] in sts:
-    #         words1 = words1[:-1]
-    word1 = sorted(words1, key=len, reverse=True)[0]
-    word2 = sorted(words2, key=len, reverse=True)[0]
-    return (fuzz.ratio(word1, word2) + fuzz.ratio(' '.join(words1), ' '.join(words2)))/2
+    return fuzz.partial_ratio(string1.upper(),string2.upper())
 
 
-def match(ocr_headers, true_headers, map_dict):
+def match(headers, true_headers, map_dict):
     # Matches ocr headers to true headers
-    for ocr_header in ocr_headers:
-        score_list = [(score(ocr_header, true_header), true_header) for true_header in true_headers]
-        sorted_score = sorted(score_list, key = lambda tup: tup[0])
-        score_tuple = sorted_score[0]
-        map_dict[ocr_header] = score_tuple
+    for header in headers:
+        if header not in map_dict:
+            score_list = [(score(header, true_header), true_header) for true_header in true_headers]
+            sorted_score = sorted(score_list, key = lambda tup: tup[0])
+            score_tuple = sorted_score[0]
+            map_dict[header] = score_tuple
     return map_dict
 
 
 def driver(df):
     # Drives the header matching code
-    true_headers = list(pd.read_csv("true_headers.csv")["true_headers"]) # or to_list() ?
+    true_headers = list(pd.read_csv("RankedHeaders.csv", usecols=[2]).dropna().rename(lambda x: "true_headers", axis="columns")["true_headers"])
     hl, mpd = preprocess(df)
+    print("LOG: Obtained headers")
     cleaned_hl = clean(hl)
+    print("LOG: Cleaned headers")
     updated_mpd = match(cleaned_hl, true_headers, mpd)
-    updated_df = df.assign(score=lambda h: updated_mpd[h][0], true_header=lambda h: updated_mpd[h][1])
+    print("LOG: Match completed")
     pkl.dump(updated_mpd, open('header_match_dict.pkl', 'wb'))
+    def assign_score(D):
+        return [updated_mpd[clean_header(h)][0] for h in D.Header]
+    def assign_header(D):
+        return [updated_mpd[clean_header(h)][1] for h in D.Header]
+    updated_df = df.assign(score=assign_score, true_header=assign_header)
     return updated_df
 
 
-
+print(driver(pd.read_csv("FOutput.csv"))[["Header", "true_header", "score"]])
